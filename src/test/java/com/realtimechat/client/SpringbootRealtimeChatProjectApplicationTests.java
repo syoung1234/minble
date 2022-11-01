443c78dfe7fde1574a187bb6068d171918461424
syoung1234 <syoung8818@gmail.com>
2022-11-01 18:49:43 +0900

    init project

---
+++ src/test/java/com/realtimechat/client/SpringbootRealtimeChatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtimechat.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRealtimeChatProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
