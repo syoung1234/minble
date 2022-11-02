656fe2ac477bdf075b65ac7c438c121058cfe20d
syoung1234 <syoung8818@gmail.com>
2022-11-02 22:03:57 +0900

    Add Anotation EnableJpaAuditing

--- src/main/java/com/realtimechat/client/SpringbootRealtimeChatProjectApplication.java
+++ src/main/java/com/realtimechat/client/SpringbootRealtimeChatProjectApplication.java
@@ -2,7 +2,9 @@ package com.realtimechat.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringbootRealtimeChatProjectApplication {
 
