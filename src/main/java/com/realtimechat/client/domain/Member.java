7e5d0c235112a301370911067829f9648b2b1ad3
syoung1234 <syoung8818@gmail.com>
2022-11-03 15:33:58 +0900

    Fix Constructor Anotation

--- src/main/java/com/realtimechat/client/domain/Member.java
+++ src/main/java/com/realtimechat/client/domain/Member.java
@@ -1,32 +1,42 @@
 package com.realtimechat.client.domain;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Builder
 public class Member extends BaseTimeEntity implements UserDetails {
     @Id
     @Column(name = "member_id")
     @GeneratedValue
     private UUID id;
-    @Column(length = 100)
+    @Column(length = 100, unique = true, nullable = false)
     private String email;
     private String password;
     private String nickname;
@@ -37,10 +47,16 @@ public class Member extends BaseTimeEntity implements UserDetails {
     private boolean notification;
 
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
     @Override
     public String getPassword() {
