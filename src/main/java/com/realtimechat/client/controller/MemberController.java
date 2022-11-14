2e4c5f5bafe84e3069dc5185f2e61b913a2c41ea
syoung1234 <syoung8818@gmail.com>
2022-11-14 20:01:35 +0900

    Add email, nickname duplication

--- src/main/java/com/realtimechat/client/controller/MemberController.java
+++ src/main/java/com/realtimechat/client/controller/MemberController.java
@@ -1,7 +1,6 @@
 package com.realtimechat.client.controller;
 
 import java.util.Map;
-import java.util.UUID;
 
 import com.realtimechat.client.config.security.JwtTokenProvider;
 import com.realtimechat.client.domain.Member;
@@ -9,6 +8,7 @@ import com.realtimechat.client.domain.Role;
 import com.realtimechat.client.repository.MemberRepository;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,15 +28,15 @@ public class MemberController {
     // 회원가입
     @PostMapping("/register")
     public String register(@RequestBody Map<String, String> user) {
-        String message = "success";
-        memberRepository.save(Member.builder()
+        Member member = memberRepository.save(Member.builder()
                 .email(user.get("email"))
                 .password(passwordEncoder.encode(user.get("password")))
                 .nickname(user.get("nickname"))
                 .phone(user.get("phone"))
                 .role(Role.ROLE_MEMBER)
                 .build());
-        return message;
+        
+        return jwtTokenProvider.createToken(member.getEmail(), member.getRole());
     } 
 
     // 로그인
@@ -50,6 +50,26 @@ public class MemberController {
 
         return jwtTokenProvider.createToken(member.getEmail(), member.getRole());
     }
+
+    // 이메일, 닉네임 중복 확인
+    @PostMapping("duplicate/{type}")
+    public String duplicate(@PathVariable String type, @RequestBody Map<String, String> user) {
+        String message = "exist";
+        Member member = null;
+        if (type.equals("email")) {
+            member = memberRepository.findByEmail(user.get("email")).orElse(null);
+        
+        } else if (type.equals("nickname")) {
+            member = memberRepository.findByNickname(user.get("nickname"));
+        }
+
+        if (member == null) {
+            message = "available";
+        }
+        
+        return message;
+    }
+
 
     
 }
