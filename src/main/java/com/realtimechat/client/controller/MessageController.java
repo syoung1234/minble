b4c2239045dd560c2f720f215b8e6d1798f51653
syoung1234 <syoung8818@gmail.com>
2022-11-29 22:05:30 +0900

    Move socket handler to message controller

--- src/main/java/com/realtimechat/client/controller/MessageController.java
+++ src/main/java/com/realtimechat/client/controller/MessageController.java
@@ -1,26 +1,36 @@
 package com.realtimechat.client.controller;
 
 import com.realtimechat.client.config.security.SecurityUser;
+import com.realtimechat.client.dto.request.MessageRequestDto;
 import com.realtimechat.client.dto.response.MessageResponseDto;
 import com.realtimechat.client.service.MessageService;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/message")
 public class MessageController {
 
     private final MessageService messageService;
+    private final SimpMessagingTemplate simpMessagingTemplate;
 
-    @GetMapping("/{nickname}")
+    @MessageMapping("/receive")
+    @SendTo("/send")
+    public void SocketHandler(MessageRequestDto messageRequestDto) {
+        simpMessagingTemplate.convertAndSend("/send/" + messageRequestDto.getChannel(), messageRequestDto);
+        //return socketDto;
+    }
+
+    @GetMapping("/api/message/{nickname}")
     public ResponseEntity<MessageResponseDto> get(@AuthenticationPrincipal SecurityUser principal, @PathVariable String nickname) {
         MessageResponseDto response = messageService.get(principal.getMember(), nickname);
 
