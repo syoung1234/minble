fc0b5a569f88dc5956044279e4a90085a35e39c2
syoung1234 <syoung8818@gmail.com>
2022-12-07 19:59:32 +0900

    Add PostMapping

--- src/main/java/com/realtimechat/client/controller/SubscriberController.java
+++ src/main/java/com/realtimechat/client/controller/SubscriberController.java
@@ -1,10 +1,14 @@
 package com.realtimechat.client.controller;
 
+import java.util.Map;
+
 import com.realtimechat.client.config.security.SecurityUser;
 import com.realtimechat.client.service.SubscriberService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,13 +22,15 @@ public class SubscriberController {
     private final SubscriberService subscriberService;
 
     // 구독 
-    public ResponseEntity<String> save(@AuthenticationPrincipal SecurityUser principal, String nickname) {
-        String response = subscriberService.save(principal.getMember(), nickname);
+    @PostMapping
+    public ResponseEntity<String> save(@AuthenticationPrincipal SecurityUser principal, @RequestBody Map<String, String> body) {
+        String response = subscriberService.save(principal.getMember(), body.get("nickname"));
 
         return ResponseEntity.ok(response);
     }
 
     // 구독 취소
+    @PostMapping("/cancel")
     public ResponseEntity<String> cancel(@AuthenticationPrincipal SecurityUser principal, String nickname) {
         String response = subscriberService.cancel(principal.getMember(), nickname);
 
