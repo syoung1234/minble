1a14e2a34586fa2c13d1473cf4ef4640de3165c3
syoung1234 <syoung8818@gmail.com>
2023-01-03 20:16:33 +0900

    Move iamport code from SubcriberController to Iamport

--- src/main/java/com/realtimechat/client/controller/SubscriberController.java
+++ src/main/java/com/realtimechat/client/controller/SubscriberController.java
@@ -1,21 +1,10 @@
 package com.realtimechat.client.controller;
 
-
-import java.util.HashMap;
-import java.util.Map;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.realtimechat.client.config.security.SecurityUser;
 import com.realtimechat.client.dto.request.PaymentRequestDto;
 import com.realtimechat.client.dto.response.SubscriberResponseDto;
 import com.realtimechat.client.service.SubscriberService;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import lombok.RequiredArgsConstructor;
 
@@ -48,79 +36,6 @@ public class SubscriberController {
         String response = subscriberService.save(principal.getMember(), body);
 
         return ResponseEntity.ok(response);
-    }
-
-    @Value("${imp.key}")
-    private String imp_key;
-
-    @Value("${imp.secret}")
-    private String imp_secret;
-
-    // test
-    @PostMapping("/test")
-    public Object iamport() {
-
-        /******************* 토큰 발급 *******************/
-        // 인증 토큰 발급 받기
-        Map<String, String> req = new HashMap<>();
-        req.put("imp_key", imp_key);
-        req.put("imp_secret", imp_secret);
-
-        String url = "https://api.iamport.kr/users/getToken";
-
-        RequestEntity<Map<String, String>> requestEntity = RequestEntity
-                .post(url)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(req);
-
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<JSONObject> response = restTemplate.exchange(requestEntity, JSONObject.class);
-
-        System.out.println(response);
-        System.out.println(response.getBody());
-
-        JSONParser jsonParser = new JSONParser();
-        ObjectMapper mapper = new ObjectMapper();
-
-        String accessToken = null;
-        // object -> json 변환 
-        try {
-            String jsonStr = mapper.writeValueAsString(response.getBody().get("response"));
-            System.out.println(jsonStr);
-
-            JSONObject jsonObject = (JSONObject) jsonParser.parse(jsonStr);
-            System.out.println(jsonObject.get("access_token"));
-            accessToken = jsonObject.get("access_token").toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-
-        /******************* 결제 요청 *******************/
-        // header
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", MediaType.APPLICATION_JSON_VALUE);
-        headers.add("Authorization", accessToken);
-
-        // body
-        Map<String, String> payReq = new HashMap<>();
-        payReq.put("customer_uid", "");
-        payReq.put("merchant_uid", "");
-        payReq.put("amount", "3300");
-        payReq.put("name", "Message 구독");
-
-        String payUrl = "https://api.iamport.kr/users/getToken";
-
-        RequestEntity<Map<String, String>> payRequestEntity = RequestEntity
-                .post(payUrl)
-                .headers(headers)
-                .body(payReq);
-
-        RestTemplate payRestTemplate = new RestTemplate();
-        ResponseEntity<JSONObject> payResponse = payRestTemplate.exchange(payRequestEntity, JSONObject.class);
-
-        return response.getBody();
-    
     }
 
     // 구독 취소
