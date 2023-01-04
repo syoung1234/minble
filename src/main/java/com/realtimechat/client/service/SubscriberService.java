77712f600d89be2911e8c211cd37a62c4ab71c75
syoung1234 <syoung8818@gmail.com>
2023-01-04 22:20:42 +0900

    Add iamport payment

--- src/main/java/com/realtimechat/client/service/SubscriberService.java
+++ src/main/java/com/realtimechat/client/service/SubscriberService.java
@@ -2,17 +2,25 @@ package com.realtimechat.client.service;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.realtimechat.client.domain.ChatRoom;
 import com.realtimechat.client.domain.Member;
+import com.realtimechat.client.domain.Payment;
 import com.realtimechat.client.domain.Subscriber;
+import com.realtimechat.client.dto.SubscriberDto;
 import com.realtimechat.client.dto.request.PaymentRequestDto;
 import com.realtimechat.client.dto.request.SubscriberRequestDto;
 import com.realtimechat.client.dto.response.SubscriberResponseDto;
 import com.realtimechat.client.repository.ChatRoomRepository;
 import com.realtimechat.client.repository.PaymentRepository;
 import com.realtimechat.client.repository.SubscriberRepository;
+import com.realtimechat.client.util.Iamport;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,6 +31,12 @@ public class SubscriberService {
     private final SubscriberRepository subscriberRepository;
     private final ChatRoomRepository chatRoomRepository;
     private final PaymentRepository paymentRepository;
+
+    @Value("${imp.key}")
+    private String imp_key;
+
+    @Value("${imp.secret}")
+    private String imp_secret;
 
     // 페이지 조회
     public SubscriberResponseDto get(Member member, String name) {
@@ -44,31 +58,72 @@ public class SubscriberService {
 
 
     // 구독
-    public String save(Member member, PaymentRequestDto paymentRequestDto) {
+    @Transactional
+    public String save(Member member, SubscriberRequestDto subscriberRequestDto) {
         String message = "success";
-        String nickname = paymentRequestDto.getNickname();
-        
-        // 결제 저장 
-        paymentRequestDto.setMember(member);
-        paymentRepository.save(paymentRequestDto.toEntity());
+        String nickname = subscriberRequestDto.getNickname();
+        String merchant_uid = member.getNickname() + "_" + LocalDateTime.now();
 
         ChatRoom chatRoom = chatRoomRepository.findByChannel(nickname);
-        LocalDateTime expiredAt = LocalDateTime.now().plusMonths(1);
-
         Subscriber subscriber = subscriberRepository.findByMemberAndChatRoom(member, chatRoom);
 
-
-        if (subscriber == null) { // 생성
-            SubscriberRequestDto subscriberRequestDto = new SubscriberRequestDto(member, chatRoom, expiredAt);
-            subscriberRepository.save(subscriberRequestDto.toEntity());
-        } else { // 업데이트 
-            if (subscriber.isStatus() == true) {
+        // 구독 여부
+        if (subscriber != null) {
+            // 구독 중이거나 구독 취소를 했지만 유효기간이 남았을 경우 
+            if (subscriber.isStatus() == true || subscriber.getExpiredAt().isBefore(LocalDateTime.now())) {
                 message = "already registered";
-            } else {
-                subscriber.update(expiredAt);
+                return message;
             }
-            
         }
+
+        Iamport iamport = new Iamport();
+        JSONObject body = (JSONObject) iamport.subscriber(imp_key, imp_secret, subscriberRequestDto.getCustomer_uid(), merchant_uid);
+
+        if (body.get("code").equals(0)) {
+            System.out.println(body.get("response"));
+            ObjectMapper mapper = new ObjectMapper();
+            JSONParser jsonParser = new JSONParser();
+
+            try {
+                String jsonStr = mapper.writeValueAsString(body.get("response"));
+                JSONObject paymentData = (JSONObject) jsonParser.parse(jsonStr);
+                String status = paymentData.get("status").toString();
+                System.out.println(paymentData);
+                String cardName = null;
+                if (status.equals("paid")) { // 결제 완료
+                    // 결제 저장 
+                    PaymentRequestDto paymentRequestDto = new PaymentRequestDto(member, paymentData);
+
+                    if (paymentData.get("card_name") != null) {
+                        paymentRequestDto.setCardName(cardName);
+                    }
+    
+                    Payment payment = paymentRepository.save(paymentRequestDto.toEntity());
+    
+                    // 유효기간 
+                    LocalDateTime expiredAt = LocalDateTime.now().plusMonths(1);
+    
+                    if (subscriber == null) { // 생성
+                        SubscriberDto subscriberDto = new SubscriberDto(member, chatRoom, expiredAt, subscriberRequestDto.getCustomer_uid());
+                        subscriberRepository.save(subscriberDto.toEntity());
+                    } else { // 업데이트 
+                        subscriber.update(expiredAt);
+                    }
+    
+                    payment.update(subscriber);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            
+             
+        } else {
+            // 결제 실패 
+            message = "fail";
+        }
+        
         
         return message;
     }
