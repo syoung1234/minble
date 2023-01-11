ce761ec1fdbc96aaaf74e3606e3f0c44af4014d5
syoung1234 <syoung8818@gmail.com>
2023-01-11 21:33:34 +0900

    Add get to AdminMember

--- src/main/java/com/realtimechat/client/controller/Admin/AdminMemberController.java
+++ src/main/java/com/realtimechat/client/controller/Admin/AdminMemberController.java
@@ -1,11 +1,16 @@
 package com.realtimechat.client.controller.Admin;
 
+import com.realtimechat.client.dto.request.admin.AdminMemberRequestDto;
 import com.realtimechat.client.dto.response.admin.AdminMemberResponseDto;
 import com.realtimechat.client.service.Admin.AdminMemberService;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +23,20 @@ public class AdminMemberController {
 
     private final AdminMemberService adminMemberService;
 
+    // admin - 회원관리
     @GetMapping
-    public Page<AdminMemberResponseDto> list(Pageable pageable) {
-        return adminMemberService.list(pageable);
+    public ResponseEntity<Page<AdminMemberResponseDto>> list(Pageable pageable) {
+        Page<AdminMemberResponseDto> response = adminMemberService.list(pageable);
+        return ResponseEntity.ok(response);
     }
+
+    // admin - 회원관리 상세
+    @GetMapping("/{nickname}")
+    public ResponseEntity<AdminMemberResponseDto> get(@PathVariable String nickname) {
+        AdminMemberResponseDto response = adminMemberService.get(nickname);
+
+        return ResponseEntity.ok(response);
+    }
+
     
 }
--- src/main/java/com/realtimechat/client/service/Admin/AdminMemberService.java
+++ src/main/java/com/realtimechat/client/service/Admin/AdminMemberService.java
@@ -1,8 +1,14 @@
 package com.realtimechat.client.service.Admin;
 
+import com.realtimechat.client.domain.ChatRoom;
 import com.realtimechat.client.domain.Member;
+import com.realtimechat.client.domain.Role;
+import com.realtimechat.client.dto.request.MessageRequestDto;
+import com.realtimechat.client.dto.request.admin.AdminMemberRequestDto;
 import com.realtimechat.client.dto.response.admin.AdminMemberResponseDto;
+import com.realtimechat.client.repository.ChatRoomRepository;
 import com.realtimechat.client.repository.MemberRepository;
+import com.realtimechat.client.repository.MessageRepository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,7 +21,10 @@ import lombok.RequiredArgsConstructor;
 public class AdminMemberService {
 
     private final MemberRepository memberRepository;
+    private final ChatRoomRepository chatRoomRepository;
+    private final MessageRepository messageRepository;
 
+    // admin 회원관리
     public Page<AdminMemberResponseDto> list(Pageable pageable) {
         Page<Member> memberList = memberRepository.findAll(pageable);
 
@@ -23,5 +32,15 @@ public class AdminMemberService {
 
         return adminMemberResponseDto;
     }
-    
+
+    // admin 회원관리 상세 
+    public AdminMemberResponseDto get(String nickname) {
+        Member member = memberRepository.findByNickname(nickname);
+
+        AdminMemberResponseDto adminMemberResponseDto = new AdminMemberResponseDto(member);
+
+        return adminMemberResponseDto;
+    }
+
+   
 }
