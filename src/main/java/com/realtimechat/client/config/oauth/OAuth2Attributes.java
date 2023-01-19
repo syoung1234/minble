bcf6971b91c50fee25ec4a82bdaec1cea9d34ac4
syoung1234 <syoung8818@gmail.com>
2023-01-19 18:54:32 +0900

    feat: rename OAuthAttributes to OAuth2Attributes

--- src/main/java/com/realtimechat/client/config/oauth/OAuth2Attributes.java
+++ src/main/java/com/realtimechat/client/config/oauth/OAuth2Attributes.java
@@ -9,37 +9,38 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class OAuthAttributes {
+public class OAuth2Attributes {
     private Map<String, Object> attributes; // OAtuh2 반환하는 유저 정보 Map
     private String nameAttributeKey;
     private String nickname;
     private String email;
-    private String picture;
+    private String social;
 
     @Builder
-    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String nickname, String email, String picture) {
+    public OAuth2Attributes(Map<String, Object> attributes, String nameAttributeKey, String nickname, String email, String social) {
         this.attributes = attributes;
         this.nameAttributeKey = nameAttributeKey;
         this.nickname = nickname;
         this.email = email;
-        this.picture = picture;
+        this.social = social;
     }
 
-    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+    public static OAuth2Attributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
         System.out.println(registrationId);
         if (registrationId.equals("kakao")) {
         }
         return ofKakao(userNameAttributeName, attributes);
     }
     
-    public static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+    public static OAuth2Attributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
         System.out.println(attributes);
         Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
         Map<String, Object> kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
 
-        return OAuthAttributes.builder()
+        return OAuth2Attributes.builder()
             .nickname((String) kakaoProfile.get("nickname"))
             .email((String) kakaoAccount.get("email"))
+            .social("kakao")
             .nameAttributeKey(userNameAttributeName)
             .attributes(attributes)
             .build();
@@ -49,6 +50,7 @@ public class OAuthAttributes {
         return Member.builder()
                 .nickname(nickname)
                 .email(email)
+                .social(social)
                 .role(Role.ROLE_MEMBER)
                 .build();
     }
