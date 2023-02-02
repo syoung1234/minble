57f71a8842079511d5e563f9c065e68f96a68cec
syoung1234 <syoung8818@gmail.com>
2023-02-02 20:28:42 +0900

    feat: add sendPasswordEmail

--- src/main/java/com/realtimechat/client/controller/EmailTokenController.java
+++ src/main/java/com/realtimechat/client/controller/EmailTokenController.java
@@ -1,6 +1,7 @@
 package com.realtimechat.client.controller;
 
 import java.util.Map;
+import java.util.Optional;
 
 import com.realtimechat.client.domain.Member;
 import com.realtimechat.client.repository.MemberRepository;
@@ -18,24 +19,39 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/email/confirm")
+@RequestMapping("/api/email")
 public class EmailTokenController {
     private final EmailTokenService emailTokenService;
     private final MemberRepository memberRepository;
 
-    @GetMapping
+    // 회원가입 이메일 인증 완료
+    @GetMapping("/confirm")
     public ResponseEntity<String> confirmEmail(@RequestParam String token) {
         String message = emailTokenService.confirmEmail(token);
 
         return ResponseEntity.ok(message);
     }
 
-    @PostMapping
+    // 회원가입 이메일 재전송
+    @PostMapping("/confirm")
     public ResponseEntity<String> resend(@RequestBody Map<String, String> body) {
         Member member = memberRepository.findByEmailAndSocial(body.get("email"), null).orElse(null);
         String receiverEmail = member.getEmail();
-        emailTokenService.createEmailToken(member, receiverEmail);
+        emailTokenService.createEmailToken(member, receiverEmail, null);
 
         return ResponseEntity.ok("success");
     }
+
+    // 비밀번호 찾기 이메일 전송
+    @GetMapping("/password")
+    public ResponseEntity<String> sendPasswordEmail(@RequestParam String email) {
+        Optional<Member> member = memberRepository.findByEmailAndSocial(email, null);
+        if (!member.isPresent()) { // 회원가입된 유저가 아님 
+            return ResponseEntity.ok("empty");
+        } else {
+            emailTokenService.createEmailToken(member.get(), email, "password");
+            return ResponseEntity.ok("success");
+        }
+    }
+
 }
