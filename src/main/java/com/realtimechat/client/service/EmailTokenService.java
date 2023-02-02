db0c6d3ddd0cafb65beff85399b1c1f67fe0baed
syoung1234 <syoung8818@gmail.com>
2023-02-02 20:21:57 +0900

    feat: add resetPassword

--- src/main/java/com/realtimechat/client/service/EmailTokenService.java
+++ src/main/java/com/realtimechat/client/service/EmailTokenService.java
@@ -10,6 +10,7 @@ import com.realtimechat.client.repository.MemberRepository;
 import com.realtimechat.client.util.MailHandler;
 
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
@@ -23,6 +24,7 @@ public class EmailTokenService {
     private final EmailTokenRepository emailTokenRepository;
     private final MemberRepository memberRepository;
     private final JavaMailSender javaMailSender;
+    private final PasswordEncoder passwordEncoder;
     
     // 이메일 인증 토큰 생성
     public String createEmailToken(Member member, String receiverEmail, String type) {
@@ -39,7 +41,6 @@ public class EmailTokenService {
         if (type == "password") { // 비밀번호 찾기
             subject = "minble 비밀번호 찾기";
             content = "<p>아래의 링크를 클릭하면 비밀번호 재설정 페이지로 이동합니다.</p> <a href='http://localhost:3000/find-password?token="+emailToken.getId()+"'>http://localhost:3000/find-password</a>";
-            
         } else { // 회원가입 이메일 인증
             subject = "minble 회원가입 이메일 인증";
             content = "<p>아래의 링크를 클릭하면 가입이 완료됩니다.</p> <a href='http://localhost:3000/confirm-email?token="+emailToken.getId()+"'>http://localhost:3000/confirm-email</a>";
@@ -90,4 +91,19 @@ public class EmailTokenService {
         return message;
     }
 
+    // 비밀번호 재설정
+    @Transactional
+    public String resetPassword(String token, String password) {
+        String message = "success";
+        EmailToken emailToken = this.findByIdAndExpirationDateAfterAndExpired(token).orElse(null);
+        if (emailToken == null) {
+            message = "fail";
+        } else {
+            Member member = memberRepository.findById(emailToken.getMember().getId()).orElse(null);
+            member.updatePassword(passwordEncoder.encode(password)); // 비밀번호 변경
+            memberRepository.save(member);
+        }
+
+        return message;
+    }
 }
