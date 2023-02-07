94bbf2cab5bb253adc485ca15ba9725e34acfd1a
syoung1234 <syoung8818@gmail.com>
2023-02-07 22:29:46 +0900

    feat: delete message_id

--- src/main/java/com/realtimechat/client/domain/MessageFile.java
+++ src/main/java/com/realtimechat/client/domain/MessageFile.java
@@ -3,9 +3,8 @@ package com.realtimechat.client.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -18,13 +17,9 @@ import lombok.Setter;
 @Entity
 public class MessageFile extends BaseTimeEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "image_file_id")
     private Integer id;
-
-    @OneToOne
-    @JoinColumn(name = "message_id")
-    private Message message;
 
     @Column(name = "file_name", length = 100)
     private String filename;
@@ -43,7 +38,6 @@ public class MessageFile extends BaseTimeEntity {
 
     @Builder
     public MessageFile(Message message, String filename, String originalFileName, String filePath, Long fileSize, String fileType) {
-        this.message = message;
         this.filename = filename;
         this.originalFileName = originalFileName;
         this.filePath = filePath;
