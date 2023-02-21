e61837088a54805bdd5b4b6f367c28d7867bfb77
syoung1234 <syoung8818@gmail.com>
2023-02-21 22:56:25 +0900

    feat: add RefreshToken entity

---
+++ src/main/java/com/realtimechat/client/domain/RefreshToken.java
@@ -0,0 +1,37 @@
+package com.realtimechat.client.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class RefreshToken extends BaseTimeEntity {
+    @Id
+    @Column(name = "refresh_id")
+    private String id;
+    
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "expiration_date")
+    private LocalDateTime expirationDate;
+
+    @Builder
+    public RefreshToken(String id, Member member, LocalDateTime expirationDate) {
+        this.id = id;
+        this.member = member;
+        this.expirationDate = expirationDate; 
+    }
+}
