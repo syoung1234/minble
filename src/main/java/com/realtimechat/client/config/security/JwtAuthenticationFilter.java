24d9a1f2f9cb3ea1929642c1f2d123852ff19ecc
syoung1234 <syoung8818@gmail.com>
2023-02-26 18:11:56 +0900

    feat: add setHeader

--- src/main/java/com/realtimechat/client/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/realtimechat/client/config/security/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,8 +22,16 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        // httpServletResponse.setHeader("Access-Control-Allow-Origin", "localhost:3000");
+        // httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        // httpServletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Origin,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization, X-AUTH-TOKEN");
+        // httpServletResponse.setHeader("Access-Control-Allow-Credentials",  "true");
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        
         // 헤더에서 JWT를 받아옴
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        String token = jwtTokenProvider.resolveToken(httpServletRequest);
         // 유효한 토큰인지 확인
         if (token != null && jwtTokenProvider.validateToken(token)) {
             // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옴
@@ -30,8 +39,8 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
             // SecurityContext에 Authentication 객체를 저장
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        }
-        chain.doFilter(request, response);
+        } 
+        chain.doFilter(request, httpServletResponse);
     }
     
 }
