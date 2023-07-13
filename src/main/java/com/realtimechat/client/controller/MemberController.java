639199730f7ed06942d41b5298393beee6c339b0
syoung1234 <syoung8818@gmail.com>
2023-07-13 18:30:30 +0900

    refactor: modify Map -> requestDto

--- src/main/java/com/realtimechat/client/controller/MemberController.java
+++ src/main/java/com/realtimechat/client/controller/MemberController.java
@@ -5,22 +5,19 @@ import java.util.Map;
 
 import com.realtimechat.client.config.security.SecurityUser;
 import com.realtimechat.client.domain.Member;
-import com.realtimechat.client.domain.Role;
 import com.realtimechat.client.dto.request.SocialRegisterRequestDto;
 import com.realtimechat.client.dto.request.member.DuplicateRequestDto;
+import com.realtimechat.client.dto.request.member.LoginRequestDto;
 import com.realtimechat.client.dto.request.member.RegisterRequestDto;
 import com.realtimechat.client.dto.response.LoginResponseDto;
 import com.realtimechat.client.dto.response.MemberResponseDto;
-import com.realtimechat.client.repository.MemberRepository;
 import com.realtimechat.client.service.EmailTokenService;
 import com.realtimechat.client.service.MemberService;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,14 +32,13 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api")
 public class MemberController {
 
-    private final MemberRepository memberRepository;
     private final MemberService memberService;
     private final EmailTokenService emailTokenService;
 
     // 로그인
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDto> login(@RequestBody Map<String, String> user) {
-        LoginResponseDto response = memberService.login(user);
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+        LoginResponseDto response = memberService.login(loginRequestDto);
         
         ResponseCookie cookie = ResponseCookie.from("refreshToken", response.getRefreshToken())
             .maxAge(30 * 24 * 60 * 60) // 30일
