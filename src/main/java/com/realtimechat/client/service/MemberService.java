7be6e270dade7945da7aa60edf178f81ae5460b2
syoung1234 <syoung8818@gmail.com>
2023-07-13 18:29:28 +0900

    feat: add MemberException

--- src/main/java/com/realtimechat/client/service/MemberService.java
+++ src/main/java/com/realtimechat/client/service/MemberService.java
@@ -8,6 +8,7 @@ import com.realtimechat.client.domain.Member;
 import com.realtimechat.client.domain.Role;
 import com.realtimechat.client.dto.request.SocialRegisterRequestDto;
 import com.realtimechat.client.dto.request.member.DuplicateRequestDto;
+import com.realtimechat.client.dto.request.member.LoginRequestDto;
 import com.realtimechat.client.dto.request.member.RegisterRequestDto;
 import com.realtimechat.client.dto.response.FollowResponseDto;
 import com.realtimechat.client.dto.response.LoginResponseDto;
@@ -22,6 +23,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -35,17 +37,22 @@ public class MemberService {
     @Value("${default.profile.path}")
     private String defaultProfilePath;
 
-    // 로그인
-    public LoginResponseDto login(Map<String, String> user) {
-        Member member = memberRepository.findByEmailAndSocial(user.get("email"), null)
-                        .orElseThrow(() -> new IllegalArgumentException("가입 되지 않은 이메일입니다."));
-        if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
-            throw new IllegalArgumentException("이메일 또는 비밀번호가 맞지 않습니다.");
+    /**
+     * 로그인
+     * @param requestDto (email, password)
+     * @return loginResponseDto, MemberException
+     */
+    public LoginResponseDto login(LoginRequestDto requestDto) {
+        Member member = memberRepository.findByEmail(requestDto.getEmail())
+                        .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND)); // 가입X
+
+        if (!passwordEncoder.matches(requestDto.getPassword(), member.getPassword())) { // 비밀번호 틀림
+            throw new MemberException(MemberErrorCode.MEMBER_NOT_FOUND);
         }
 
         LoginResponseDto loginResponseDto;
-        if (member.isEmailConfirmation() == false) { // 이메일 인증이 안 된 회원 
-            loginResponseDto = new LoginResponseDto(null, null, "unconfirmed", null, null);
+        if (!member.isEmailConfirmation()) { // 이메일 인증이 안 된 회원
+            throw new MemberException(MemberErrorCode.UNAUTHORIZED_MEMBER);
         } else {
             String accessToken = jwtTokenProvider.createToken(member.getEmail(), member.getRole(), null);
             String refreshToken = jwtTokenProvider.createRefreshToken(member);
@@ -55,7 +62,12 @@ public class MemberService {
         return loginResponseDto;
     }
 
-    // 회원가입
+    /**
+     * 회원가입
+     * @param registerRequestDto (email, password, nickname)
+     * @return member
+     */
+    @Transactional
     public Member register(RegisterRequestDto registerRequestDto) {
         Member member = Member.builder()
                 .email(registerRequestDto.getEmail())
@@ -68,18 +80,27 @@ public class MemberService {
         return memberRepository.save(member);
     }
 
-    // 소셜 회원가입
+    /**
+     * 소셜 회원가입 (네이버, 카카오, 구글)
+     * @param socialRegisterRequestDto (email, password, social)
+     * @return LoginResponseDto
+     */
+    @Transactional
     public LoginResponseDto socialSave(SocialRegisterRequestDto socialRegisterRequestDto) {
         Member member = memberRepository.findByEmailAndSocial(socialRegisterRequestDto.getEmail(), socialRegisterRequestDto.getSocial()).orElse(null);
 
         String accessToken = jwtTokenProvider.createToken(member.getNickname(), member.getRole(), member.getSocial());
         String refreshToken = jwtTokenProvider.createRefreshToken(member);
-        LoginResponseDto loginResponseDto = new LoginResponseDto(accessToken, refreshToken, "success", member.getRole().toString(), member.getNickname());
 
-        return loginResponseDto;
+        return new LoginResponseDto(accessToken, refreshToken, "success", member.getRole().toString(), member.getNickname());
     }
 
-    // 이메일, 닉네임 중복 검사
+    /**
+     * 이메일 또는 닉네임 중복 검사
+     * @param duplicateRequestDto (email, nickname)
+     * @param type (email, nickname)
+     * @return message, MemberException
+     */
     public String duplicate(DuplicateRequestDto duplicateRequestDto, String type) {
         String message = "available";
         Optional<Member> member = Optional.of(new Member());
