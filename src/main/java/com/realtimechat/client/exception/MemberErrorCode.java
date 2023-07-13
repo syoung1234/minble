13982b560d06649d86f88cc56692dd6ca481bc4c
syoung1234 <syoung8818@gmail.com>
2023-07-13 18:38:33 +0900

    feat: add unauthorized

--- src/main/java/com/realtimechat/client/exception/MemberErrorCode.java
+++ src/main/java/com/realtimechat/client/exception/MemberErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum MemberErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "Member Not found"),
-    DUPLICATED_MEMBER(HttpStatus.BAD_REQUEST, "Duplicated Member Register Request");
+    DUPLICATED_MEMBER(HttpStatus.BAD_REQUEST, "Duplicated Member"),
+    UNAUTHORIZED_MEMBER(HttpStatus.UNAUTHORIZED, "Unauthorized");
+
 
     private final HttpStatus httpStatus;
     private final String message;
