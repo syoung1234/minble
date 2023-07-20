4ae9723ff4975cdb3c7a9d06862296f702cefbff
syoung1234 <syoung8818@gmail.com>
2023-07-20 20:41:52 +0900

    feat: add PostException

--- src/main/java/com/realtimechat/client/exception/ErrorCode.java
+++ src/main/java/com/realtimechat/client/exception/ErrorCode.java
@@ -7,10 +7,13 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
+    // Member
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "Member Not found"),
     DUPLICATED_MEMBER(HttpStatus.BAD_REQUEST, "Duplicated Member"),
-    UNAUTHORIZED_MEMBER(HttpStatus.UNAUTHORIZED, "Unauthorized");
+    UNAUTHORIZED_MEMBER(HttpStatus.UNAUTHORIZED, "Unauthorized"),
 
+    // Post
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post Not found");
 
     private final HttpStatus httpStatus;
     private final String message;
---
+++ src/main/java/com/realtimechat/client/exception/PostException.java
@@ -0,0 +1,13 @@
+package com.realtimechat.client.exception;
+
+import lombok.Getter;
+
+@Getter
+public class PostException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public PostException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
