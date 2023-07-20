0438b3bf5addd0c2a1e48c2dd474c48dc5fb47c7
syoung1234 <syoung8818@gmail.com>
2023-07-20 19:08:52 +0900

    feat: 게시글 상세 조회 쿼리 추가

--- src/main/java/com/realtimechat/client/repository/PostRepositoryCustom.java
+++ src/main/java/com/realtimechat/client/repository/PostRepositoryCustom.java
@@ -1,16 +1,16 @@
 package com.realtimechat.client.repository;
 
 import com.realtimechat.client.domain.Member;
-import com.realtimechat.client.domain.Post;
-import com.realtimechat.client.dto.response.PostListResponseDto;
+import com.realtimechat.client.dto.response.PostResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.util.List;
 
 public interface PostRepositoryCustom {
 
     // 팔로잉한 스타 게시글 보기
-    Page<PostListResponseDto> findByPostAndFollowingAndFavoriteCountAndCommentCount(Member member, String nickname, Pageable pageable);
+    Page<PostResponseDto> findAllByPostAndFollowingAndCountCommentAndCountFavorite(Member member, String nickname, Pageable pageable);
+
+    // 게시글 상세 조회
+    PostResponseDto findByPostAndCountCommentAndCountFavorite(int postId);
 
 }
--- src/main/java/com/realtimechat/client/repository/PostRepositoryImpl.java
+++ src/main/java/com/realtimechat/client/repository/PostRepositoryImpl.java
@@ -6,7 +6,7 @@ import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.realtimechat.client.domain.*;
-import com.realtimechat.client.dto.response.PostListResponseDto;
+import com.realtimechat.client.dto.response.PostResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -21,21 +21,16 @@ public class PostRepositoryImpl implements PostRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
-    @Override
-    public Page<PostListResponseDto> findByPostAndFollowingAndFavoriteCountAndCommentCount(Member member, String nickname, Pageable pageable) {
-        QPost qPost = QPost.post;
-        QFollow qFollow = QFollow.follow;
-        QComment subComment = new QComment("subComment");
-        QFavorite subFavorite = new QFavorite("subFavorite");
-        QMember qMember = QMember.member;
-        BooleanBuilder builder = new BooleanBuilder();
-
-        JPAQuery<PostListResponseDto> list = queryFactory
+    /* 공통 쿼리: nickname, profilePath, postId, content, createdAt, subComment.count(), subFavorite.count() */
+    private JPAQuery<PostResponseDto> getPostCommonQuery(QPost qPost, QComment subComment, QFavorite subFavorite) {
+        return queryFactory
                 .select(Projections.constructor(
-                        PostListResponseDto.class,
+                        PostResponseDto.class,
                         qPost.member.nickname,
                         qPost.member.profilePath,
+                        qPost.id,
                         qPost.content,
+                        qPost.createdAt,
                         JPAExpressions.select(subComment.count())
                                 .from(subComment)
                                 .where(subComment.post.eq(qPost)),
@@ -44,6 +39,19 @@ public class PostRepositoryImpl implements PostRepositoryCustom {
                                 .where(subFavorite.post.eq(qPost))
                 ))
                 .from(qPost);
+    }
+
+    /* 팔로잉한 스타의 모든 게시글 조회 or 스타 멤버 1명 게시글 조회 */
+    @Override
+    public Page<PostResponseDto> findAllByPostAndFollowingAndCountCommentAndCountFavorite(Member member, String nickname, Pageable pageable) {
+        QPost qPost = QPost.post;
+        QFollow qFollow = QFollow.follow;
+        QComment subComment = new QComment("subComment");
+        QFavorite subFavorite = new QFavorite("subFavorite");
+        QMember qMember = QMember.member;
+        BooleanBuilder builder = new BooleanBuilder();
+
+        JPAQuery<PostResponseDto> list = getPostCommonQuery(qPost, subComment, subFavorite);
 
         if (nickname == null || nickname.equals("")) { // 팔로우한 모든 멤버 조회
             builder.and(qPost.member.eq(qFollow.following));
@@ -57,7 +65,7 @@ public class PostRepositoryImpl implements PostRepositoryCustom {
 
         list.where(builder).orderBy(qPost.createdAt.desc());
 
-        List<PostListResponseDto> content = list
+        List<PostResponseDto> content = list
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
@@ -65,6 +73,18 @@ public class PostRepositoryImpl implements PostRepositoryCustom {
         long total = list.fetch().size();
 
         return new PageImpl<>(content, pageable, total);
+    }
+
+    /* 게시글 상세 조회 */
+    @Override
+    public PostResponseDto findByPostAndCountCommentAndCountFavorite(int postId) {
+        QPost qPost = QPost.post;
+        QComment subComment = new QComment("subComment");
+        QFavorite subFavorite = new QFavorite("subFavorite");
+
+        return getPostCommonQuery(qPost, subComment, subFavorite)
+                .where(qPost.id.eq(postId))
+                .fetchFirst();
     }
 
 }
