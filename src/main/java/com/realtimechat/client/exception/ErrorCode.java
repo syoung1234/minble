f4fc1b99feaffdd198efb8d31acac46df01c7026
syoung1234 <syoung8818@gmail.com>
2023-07-25 21:28:28 +0900

    feat: add FORBIDDEN_MEMBER

--- src/main/java/com/realtimechat/client/exception/ErrorCode.java
+++ src/main/java/com/realtimechat/client/exception/ErrorCode.java
@@ -8,12 +8,13 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
     // Member
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "Member Not found"),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "Member Not Found"),
     DUPLICATED_MEMBER(HttpStatus.BAD_REQUEST, "Duplicated Member"),
-    UNAUTHORIZED_MEMBER(HttpStatus.UNAUTHORIZED, "Unauthorized"),
+    UNAUTHORIZED_MEMBER(HttpStatus.UNAUTHORIZED, "Unauthorized Member"),
+    FORBIDDEN_MEMBER(HttpStatus.FORBIDDEN, "Forbidden Member"),
 
     // Post
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post Not found");
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post Not Found");
 
     private final HttpStatus httpStatus;
     private final String message;
