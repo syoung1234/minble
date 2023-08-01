564a16f3f7b09b41757fc6710af16bafe41d4d32
syoung1234 <syoung8818@gmail.com>
2023-08-01 17:18:55 +0900

    test: repository deleteAll 추가

--- src/test/java/com/realtimechat/client/repository/RefreshTokenRepositoryTest.java
+++ src/test/java/com/realtimechat/client/repository/RefreshTokenRepositoryTest.java
@@ -2,9 +2,8 @@ package com.realtimechat.client.repository;
 
 import com.realtimechat.client.domain.Member;
 import com.realtimechat.client.domain.RefreshToken;
-import com.realtimechat.client.exception.ErrorCode;
-import com.realtimechat.client.exception.MemberException;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,18 +15,23 @@ import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootTest
-class RefreshTokenRepositoryTest {
+class RefreshTokenRepositoryTest extends TestBase {
     @Autowired
     private RefreshTokenRepository refreshTokenRepository;
     @Autowired
     private MemberRepository memberRepository;
+
+    @AfterEach
+    public void cleanUp() {
+        refreshTokenRepository.deleteAll();
+    }
 
     @Test
     @Transactional
     void save() {
         // given
         String token = UUID.randomUUID().toString();
-        Member member = memberRepository.findByNickname("test1").orElseThrow(() -> new MemberException(ErrorCode.MEMBER_NOT_FOUND));
+        Member member = memberRepository.save(createMember(10));
         LocalDateTime expirationDate = LocalDateTime.now().plusMonths(1);
         long expirationTime = Timestamp.valueOf(expirationDate).getTime();
 
