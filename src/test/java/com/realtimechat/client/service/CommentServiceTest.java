2a7d25b876720e27ac5b2ee2fc70d1672654f7dc
syoung1234 <syoung8818@gmail.com>
2023-08-08 18:17:43 +0900

    test: 댓글 저장 테스트 추가

--- src/test/java/com/realtimechat/client/service/CommentServiceTest.java
+++ src/test/java/com/realtimechat/client/service/CommentServiceTest.java
@@ -2,8 +2,16 @@ package com.realtimechat.client.service;
 
 import com.realtimechat.client.domain.Comment;
 import com.realtimechat.client.domain.Member;
+import com.realtimechat.client.domain.Post;
+import com.realtimechat.client.dto.request.CommentRequestDto;
 import com.realtimechat.client.dto.response.CommentResponseDto;
+import com.realtimechat.client.exception.CommentException;
+import com.realtimechat.client.exception.ErrorCode;
+import com.realtimechat.client.exception.PostException;
 import com.realtimechat.client.repository.CommentRepository;
+import com.realtimechat.client.repository.PostRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,8 +24,11 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,6 +37,8 @@ class CommentServiceTest {
     private CommentService commentService;
     @Mock
     private CommentRepository commentRepository;
+    @Mock
+    private PostRepository postRepository;
 
     @Test
     void find() {
@@ -48,6 +61,56 @@ class CommentServiceTest {
         // then
         assertThat(result).isNotNull();
         assertThat(result).size().isEqualTo(2);
+    }
+
+    @DisplayName("저장 실패-게시글 없음")
+    @Test
+    void save_fail() {
+        // given
+        CommentRequestDto commentRequestDto = new CommentRequestDto();
+        Member member = new Member();
+        doReturn(Optional.empty()).when(postRepository).findById(any());
+
+        // when
+        PostException postException = assertThrows(PostException.class, () -> commentService.save(commentRequestDto, member));
+
+        // then
+        assertThat(postException.getErrorCode()).isEqualTo(ErrorCode.POST_NOT_FOUND);
+
+    }
+
+    @DisplayName("답글 저장 실패 - 댓글 없음")
+    @Test
+    void reply_fail() {
+        // given
+        CommentRequestDto commentRequestDto = new CommentRequestDto();
+        commentRequestDto.setParentId(1);
+        Post post = new Post();
+        Member member = new Member();
+        doReturn(Optional.of(post)).when(postRepository).findById(any());
+        doReturn(Optional.empty()).when(commentRepository).findById(any());
+
+        // when
+        CommentException commentException = assertThrows(CommentException.class, () -> commentService.save(commentRequestDto, member));
+
+        // then
+        assertThat(commentException.getErrorCode()).isEqualTo(ErrorCode.COMMENT_NOT_FOUND);
+    }
+
+    @DisplayName("저장 성공")
+    @Test
+    void save() {
+        // given
+        CommentRequestDto commentRequestDto = new CommentRequestDto();
+        Member member = new Member();
+        Post post = new Post();
+        doReturn(Optional.of(post)).when(postRepository).findById(any());
+
+        // when
+        String result = commentService.save(commentRequestDto, member);
+
+        // then
+        assertThat(result).isEqualTo("success");
     }
 
 }
