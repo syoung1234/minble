e75d8c3d486e60cf14cc5d971293dd8cd2d9651b
syoung1234 <syoung8818@gmail.com>
2023-08-12 15:54:55 +0900

    feat: Optional 추가

--- src/main/java/com/realtimechat/client/repository/FavoriteRepository.java
+++ src/main/java/com/realtimechat/client/repository/FavoriteRepository.java
@@ -6,8 +6,10 @@ import com.realtimechat.client.domain.Post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FavoriteRepository extends JpaRepository<Favorite, Integer> {
-    Favorite findByMemberAndPost(Member member, Post post);
+    Optional<Favorite> findByMemberAndPost(Member member, Post post);
 
     Long countByPost(Post post); // 해당 게시글 좋아요 수
 }
