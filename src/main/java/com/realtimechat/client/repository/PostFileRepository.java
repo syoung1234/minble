32211cbe92fff11908867cc14c07e79c6a3353ac
syoung1234 <syoung8818@gmail.com>
2023-08-13 17:36:27 +0900

    feat: Optional 추가

--- src/main/java/com/realtimechat/client/repository/PostFileRepository.java
+++ src/main/java/com/realtimechat/client/repository/PostFileRepository.java
@@ -1,6 +1,7 @@
 package com.realtimechat.client.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.realtimechat.client.domain.Post;
 import com.realtimechat.client.domain.PostFile;
@@ -8,7 +9,7 @@ import com.realtimechat.client.domain.PostFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostFileRepository extends JpaRepository<PostFile, Integer> {
-    PostFile findByFilename(String filename);
+    Optional<PostFile> findByFilename(String filename);
 
     List<PostFile> findByPost(Post post);
 }
