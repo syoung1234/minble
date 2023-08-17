6f87432043e18d99761e613902a1ad2006d7eac7
syoung1234 <syoung8818@gmail.com>
2023-08-17 19:53:37 +0900

    refactor: MemberResponseDto constructor

--- src/main/java/com/realtimechat/client/dto/response/MemberResponseDto.java
+++ src/main/java/com/realtimechat/client/dto/response/MemberResponseDto.java
@@ -1,6 +1,7 @@
 package com.realtimechat.client.dto.response;
 
 
+import com.realtimechat.client.domain.Member;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -10,8 +11,8 @@ public class MemberResponseDto {
     private String nickname;
     private String profilePath;
 
-    public MemberResponseDto(String nickname, String profilePath) {
-        this.nickname = nickname;
-        this.profilePath = profilePath;
+    public MemberResponseDto(Member member) {
+        this.nickname = member.getNickname();
+        this.profilePath = member.getProfilePath();
     }
 }
