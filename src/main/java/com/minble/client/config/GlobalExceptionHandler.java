30d995ef815bdcc398e62de6964b0e005d215ea8
syoung1234 <syoung8818@gmail.com>
2023-10-03 15:42:46 +0900

    feat: add FollowException handler

--- src/main/java/com/minble/client/config/GlobalExceptionHandler.java
+++ src/main/java/com/minble/client/config/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.minble.client.config;
 
 import com.minble.client.exception.ErrorResponse;
+import com.minble.client.exception.FollowException;
 import com.minble.client.exception.MemberException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MemberException.class)
     public ResponseEntity<ErrorResponse> handleMemberException(MemberException exception) {
         log.error("MemberException ", exception);
+        ErrorResponse errorResponse = new ErrorResponse(exception.getErrorCode());
+
+        return new ResponseEntity<>(errorResponse, exception.getErrorCode().getHttpStatus());
+    }
+
+    @ExceptionHandler(FollowException.class)
+    public ResponseEntity<ErrorResponse> handleFollowException(FollowException exception) {
+        log.error("FollowException", exception);
         ErrorResponse errorResponse = new ErrorResponse(exception.getErrorCode());
 
         return new ResponseEntity<>(errorResponse, exception.getErrorCode().getHttpStatus());
